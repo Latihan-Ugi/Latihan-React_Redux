66287e53462a72f51856dfaf69cdf1f2c3a2d95d
ugi ispoyo widodo <ugi.ispoyowidodo@gmail.com>
2018-08-11 15:54:11 +0700

    frist commit

---
+++ src/components/Postform.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from 'react-redux'
+import { createPost } from './../actions/postActions'
+
+class Postform extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title:'',
+            body:''
+        }
+
+        this.onChangeValue = this.onChangeValue.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChangeValue(e){
+        this.setState({
+            [e.target.name]:e.target.value
+        })
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        const post = {
+            title: this.state.title,
+            body: this.state.body
+        }
+
+        // call action
+        this.props.createPost(post);
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Add Post</h1>
+                <form onSubmit={this.onSubmit}>
+                    <div>
+                        <label>Title : </label><br/>
+                        <input type="text" name="title" onChange={this.onChangeValue} value={this.state.title} />
+                    </div>
+                    <br/>
+                    <div>
+                        <label>Body : </label><br/>
+                        <textarea name="body" onChange={this.onChangeValue} value={this.state.body} />
+                    </div>
+                    <br/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+Postform.propTypes = {
+    createPost: PropTypes.func.isRequired
+}
+
+export default connect(null, { createPost })(Postform);
